6493e396a91047c5274c374b4dc78f20ab018894
Matt Schallert <mattschallert@gmail.com>
2012-11-15 22:25:40 -0500

    Code cleanup

--- app.js
+++ app.js
@@ -1,80 +1,16 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , flash = require('connect-flash')
-  , routes = require('./routes')
   , http = require('http')
   , path = require('path')
   , passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
+  , myUtil = require('./myUtil.js')
 
-var users = [
-    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
-  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
-];
+  // Routes
+  , routes = require('./routes')
+  , user = require('./routes/user');
 
-function findById(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
-
-function findByUsername(username, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
-
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-
-
-// Use the LocalStrategy within Passport.
-//   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a username and password), and invoke a callback
-//   with a user object.  In the real world, this would query a database;
-//   however, in this example we are using a baked-in set of users.
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      
-      // Find the user by username.  If there is no user with the given
-      // username, or the password is not correct, set the user to `false` to
-      // indicate failure and set a flash message.  Otherwise, return the
-      // authenticated `user`.
-      findByUsername(username, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
-        return done(null, user);
-      })
-    });
-  }
-));
+// Initialize passport with all the shit we need
+myUtil.setupPassport(passport);
 
 var app = express();
 
@@ -100,23 +36,15 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/login', function(req, res){
-  res.render('login', { user: req.user, message: req.flash('error') });
-});
 
-// POST /login
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-//
-//   curl -v -d "username=bob&password=secret" http://127.0.0.1:3000/login
+// User control
+app.get('/login', user.loginGet);
 app.post('/login', 
   passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
-  function(req, res) {
-    res.redirect('/');
-  }
+  user.loginPost
 );
+app.get('/logout', user.logout);
 
+// Fire it up
 app.listen(app.get('port'));
 console.log("Express server listening on port " + app.get('port'));
---
+++ routes/user.js
@@ -0,0 +1,12 @@
+module.exports = {
+  loginGet: function (req, res){
+    res.render('login', { user: req.user, message: req.flash('error') });
+  },
+  loginPost: function (req, res) {
+    res.redirect('/');
+  },
+  logout: function (req, res) {
+    req.logout();
+    res.redirect('/');
+  }
+}
